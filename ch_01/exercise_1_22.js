4259e3a0297266d3e9bb80b48928cede7bb71cd0
Robert Westenberger <robert.michael.westenberger@gmail.com>
2020-05-28 17:26:44 -0700

    Exercise 1.22

--- ch_01/exercise_1_22.js
+++ ch_01/exercise_1_22.js
@@ -9,16 +9,38 @@ function square(n) {
 function divides(a, b) {
   return b % a === 0;
 }
+function find_divisor_iter(n, test_divisor) {
+
+  let iter = test_divisor;
+  for(null; iter < (n); iter++) {
+    if (square(iter) > n) {
+      return n;
+    }
+    if (divides(iter, n)) {
+      return test_divisor;
+    }
+  }
+  return n;
+}
+
+// TODO: convert to iterative recursive
 function find_divisor(n, test_divisor) {
-  return square(test_divisor) > n ? n : divides(test_divisor, n) ? test_divisor : find_divisor(n, test_divisor + 1);
+  if (square(test_divisor) > n ) {
+    return n;
+  }
+  if (divides(test_divisor, n)) {
+    return test_divisor;
+  }
+  return find_divisor(n, test_divisor + 1);
 }
+
 function smallest_divisor(n) {
-  return find_divisor(n, 2);
+  return find_divisor_iter(n, 2);
 }
+
 function is_prime(n) {
-  console.time(n);
+
   const is_prime = n === smallest_divisor(n);
-  console.timeEnd(n);
   return is_prime;
 }
 //Just assume end > start
@@ -26,16 +48,19 @@ function search_for_primes(start, end) {
   const startAdjusted = start % 2 === 0 ? start + 1 : start;
   const endAdjusted = end % 2 === 0 ? end - 1 : end;
   const primesArray = [];
+  console.time("Search for primes..");
   for(i = startAdjusted; i <= endAdjusted; i +=2) {
     if (primesArray.length >= 3) {
+      console.timeEnd("Search for primes..");
       return primesArray;
     }
     if(is_prime(i)) {
       primesArray.push(i);
     }
   }
+  console.timeEnd("Search for primes..");
   return primesArray;
 }
 
-const t0 = search_for_primes(100000, 200000);
-
+const t0 = search_for_primes(10000000, 50000000);
+console.log(t0);
