cdc3c10b79ce9278b1b83634de6ea17cfbe5ca23
Robert Westenberger <robert.westenberger@healthtrio.com>
2020-08-11 18:51:21 -0700

    Exercise 2.34

--- ch_02/exercise_2.34.js
+++ ch_02/exercise_2.34.js
@@ -4,9 +4,10 @@ import {accumulate, list, pair, display } from "../general";
 
 const horner_eval = (x, coefficient_sequence) => accumulate(
     (this_coefficient, higher_terms) => {
+
         return x * higher_terms + this_coefficient;
     },
     0, coefficient_sequence);
 
 // For example, to compute 1+3x+5x3+x5 at x=2 you would evaluate
-// horner_eval(2, list(1, 3, 0, 5, 0, 1));
+horner_eval(2, list(1, 3, 0, 5, 0, 1));
