683bed383cd85dd94b4122c328eb38858e8939d6
Robert Westenberger <robert.michael.westenberger@gmail.com>
2020-08-23 21:08:30 -0700

    notes 2.3.2 continued

--- ch_02/section_2.3.2.example_symbolic_differentiation.js
+++ ch_02/section_2.3.2.example_symbolic_differentiation.js
@@ -16,13 +16,9 @@ function is_sum(x) {
     return is_pair(x) && head(x) === "+";
 }
 
-function make_sum(a1, a2) {
-    return list("+", a1, a2);
-}
 
-function make_product(m1, m2) {
-    return list("*", m1, m2);
-}
+
+
 
 function addend(s) {
     return head(tail(s));
@@ -44,29 +40,66 @@ function multiplicand(s) {
     return head(tail(tail(s)));
 }
 
+function number_equal(exp, num) {
+    return is_number(exp) && exp === num;
+}
+
+function make_product(m1, m2) {
+    return number_equal(m1, 0) || number_equal(m2, 0)
+        ? 0
+        : number_equal(m1, 1)
+            ? m2
+            : number_equal(m2, 1)
+                ? m1
+                : is_number(m1) && is_number(m2)
+                    ? m1 * m2
+                    : list("*", m1, m2);
+}
+
+function make_sum(a1, a2) {
+    return number_equal(a1, 0)
+        ? a2
+        : number_equal(a2, 0)
+            ? a1
+            : is_number(a1) && is_number(a2)
+                ? a1 + a2
+                : list("+", a1, a2);
+}
+
 const error = (msg) => {
     console.error(msg);
 }
-function deriv(exp, variable) {
-    return is_number(exp)
-        ? 0
-        : is_variable(exp)
-            ? (is_same_variable(exp, variable)) ? 1 : 0
-            : is_sum(exp)
-                ? make_sum(deriv(addend(exp), variable),
-                    deriv(augend(exp), variable))
-                : is_product(exp)
-                    ? make_sum(make_product(multiplier(exp),
-                        deriv(multiplicand(exp),
-                            variable)),
-                        make_product(deriv(multiplier(exp),
-                            variable),
-                            multiplicand(exp)))
-                    : error(exp,
-                        "unknown expression type in deriv");
+
+
+const deriv = (expression, variable) => {
+
+    if (is_number(expression)) {
+        return 0;
+    }
+    if (is_variable(expression)) {
+        if (is_same_variable(expression, variable)) {
+            return 1;
+        }
+        return 0;
+    }
+    if (is_sum(expression)) {
+        return make_sum(deriv(addend(expression), variable),
+            deriv(augend(expression), variable));
+    }
+    if (is_product(expression)) {
+        return make_sum(make_product(multiplier(expression),
+            deriv(multiplicand(expression),
+                variable)),
+            make_product(deriv(multiplier(expression),
+                variable),
+                multiplicand(expression)));
+    }
+    return error(expression,
+        "unknown expression type in deriv");
 }
 
 
+debugger;
 const derivation_01 = deriv(list("*", list("*", "x", "y"), list("+", "x", 3)), "x");
 display(derivation_01);
 
