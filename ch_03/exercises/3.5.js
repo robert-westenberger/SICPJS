29cf9e0ed0b18b881d4d10de1df59721262a2ad6
Robert Westenberger <robert.westenberger@healthtrio.com>
2020-12-18 20:58:51 -0700

    WIP Exercise 3.5

--- ch_03/exercises/3.5.js
+++ ch_03/exercises/3.5.js
@@ -22,29 +22,66 @@ import {
 } from "../../general/index";
 
 
+function estimate_pi_non_recurse(trials) {
+    // function monte_carlo_non_recurse(trials, experiment) {
+    //     function iter(trials_remaining, trials_passed) {
+    //         if (trials_remaining === 0) {
+    //             return trials_passed / trials;
+    //         } else if (experiment()) {
+    //             return iter(trials_remaining - 1,
+    //                 trials_passed + 1);
+    //         } else {
+    //             return iter(trials_remaining - 1,
+    //                 trials_passed);
+    //         }
+    //     }
+    //     return iter(trials, 0);
+    // }
+    function monte_carlo_non_recurse(trials, experiment) {
+        let trials_passed = 0;
+        for (let i = 0; i <= trials; i++) {
+            if (experiment()) trials_passed++;
+        }
 
-function estimate_pi(trials) {
-    return math_sqrt(6 / monte_carlo(trials, dirichlet_test));
+        return trials_passed / trials;
+    }
+    return math_sqrt(6 / monte_carlo_non_recurse(trials, dirichlet_test));
 }
 
 function dirichlet_test() {
     return gcd(pseudo_rand(), pseudo_rand()) === 1;
 }
-
-function monte_carlo(trials, experiment) {
-    function iter(trials_remaining, trials_passed) {
-        if (trials_remaining === 0) {
-            return trials_passed / trials;
-        } else if (experiment()) {
-            return iter(trials_remaining - 1,
-                trials_passed + 1);
-        } else {
-            return iter(trials_remaining - 1,
-                trials_passed);
+function estimate_pi(trials) {
+    function monte_carlo(trials, experiment) {
+        function iter(trials_remaining, trials_passed) {
+            if (trials_remaining === 0) {
+                return trials_passed / trials;
+            } else if (experiment()) {
+                return iter(trials_remaining - 1,
+                    trials_passed + 1);
+            } else {
+                return iter(trials_remaining - 1,
+                    trials_passed);
+            }
         }
+        return iter(trials, 0);
     }
-    return iter(trials, 0);
+    return math_sqrt(6 / monte_carlo(trials, dirichlet_test));
 }
 
-const pi = estimate_pi(10000);
 
+
+// const res = (a, b) => a && Math.abs(a - b) / a;
+// const native_pi = Math.PI;
+// // value the authors came up with.
+// const sicp_author_source_pi = 3.1408877612819492;
+// const sicp_native_delta = res(native_pi, sicp_author_source_pi);
+// const num_trials = 10000;
+// const pi = estimate_pi_non_recurse(num_trials);
+// console.log(pi);
+// const non_recurse_delta = res(pi, native_pi);
+// const pi_estimation_at_num_trials_better_than_sicp = non_recurse_delta < sicp_native_delta;
+// console.log(res(non_recurse_delta, sicp_native_delta));
+// const pi_2 = estimate_pi(num_trials);
+// console.log(pi_2, res(pi_2, native_pi));
+// console.log(res(pi, pi_2));
