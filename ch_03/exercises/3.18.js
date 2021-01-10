3f881d0bb245f90c27c9299a643d0fc857dce726
Robert Westenberger <robert.westenberger@healthtrio.com>
2021-01-09 20:15:29 -0700

    WIP exercise 3.18

---
+++ ch_03/exercises/3.18.js
@@ -0,0 +1,28 @@
+import {
+    list,
+    is_pair,
+    is_null,
+    pair,
+    head,
+    for_each, list_length, display,
+    set_tail, set_head, tail, append_mutator, make_cycle
+} from "../../general/index";
+
+
+const not_cycle = list(1, 2, 3, 4);
+const cycle = make_cycle(list(1, 2, 3, 4));
+
+
+const is_cycle = (x) => {
+    let visited_items = null;
+    const is_cycle_iter = (arg) => {
+       
+    }
+    return is_cycle_iter(x);
+}
+
+
+const t0 = is_cycle(not_cycle);
+console.log(t0);
+// const t1 = is_cycle(cycle);
+// console.log(t1);
